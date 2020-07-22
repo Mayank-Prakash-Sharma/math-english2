885f93e4f3ea95041884aa5838b72578022e1da5
Mayank-Prakash-Sharma <mayank.rnd@gmail.com>
2020-07-23 00:44:40 +0530

    Section data accessed and displayed on browser

--- src/app/test-package/tests/instructions/test/test.page.ts
+++ src/app/test-package/tests/instructions/test/test.page.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+//Import ActivatedRoute and inject into constructor (to allow us to subscribe to the dynamic id passed on by the previous page)
+import { ActivatedRoute } from '@angular/router';
+
+//Import and inject NavController
+import { NavController } from '@ionic/angular';
+
+//Import Observable
+import { Observable } from 'rxjs';
+
+//Import the data model that defines the structure of our objects in Firestore Database
+import { TestService } from './test.service';
+import { Section } from './test.model';
+
+import { InstructionsService } from '../instructions.service';
+import { Test } from '../instructions.model';
+
+
 @Component({
   selector: 'app-test',
   templateUrl: './test.page.html',
@@ -7,9 +24,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestPage implements OnInit {
 
-  constructor() { }
+   //For storing the data of the dynamic subject Id passed from the previous page
+   loadedTest: string;
+  
+  //To store the Observable received from Firebase
+  private sections: Observable<Section[]>;
+  private section: Test;
+
+  constructor(private route: ActivatedRoute, private navCtrl: NavController, private instructionsService: InstructionsService, private testService: TestService) { }
 
   ngOnInit() {
+     //If dynamic attribute has been passed, we will store it on our local variable, so that we can use it on our html template
+      this.loadedTest = this.route.snapshot.paramMap.get('testId');
+
+      //Setting the value of the loadedTest1 in .service.ts file to be the same as loadedTest
+      this.testService.loadedTest1 = this.loadedTest; 
+      
+      //Set the reference of the collection choosen by the user in the previous page
+      this.testService.setSections();
+      
+      //Store the observable received from Firebase Firestore in our local observable
+      this.sections = this.testService.getSections();
+
+      if (this.loadedTest) {
+        this.instructionsService.getTest(this.loadedTest).subscribe(section => {
+        this.section = section; 
+        });
+      }
+      console.log("abrakadabra");
+      console.log(this.testService.loadedTest1);
+
   }
 
 }
---
+++ src/app/test-package/tests/instructions/test/test.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+
+// Import the data model for loadedTest1 object. It will have the same value as loadedTest variable (which holds the dynamic test id passed from the previous page) of the .ts file. I am going to use this loadedTest1 object in my fucntion ........ to display my test specific sections. 
+import { InstructionsService } from '../instructions.service';
+import { Test } from '../instructions.model';
+import { TestsService } from '../../tests.service';
+import { TestPackage } from '../../tests.model';
+import { TestPackageService } from '../../../test-package.service';
+
+//Import the essential modules to work with Firebase Firestore database
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+//Import the data model that defines the structure of our objects in Firestore Database
+import { Section } from './test.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestService {
+  //For storing the data of the dynamic test Id passed from the previous page. I am basically doing this in .ts file. There i will just set the value of this variable. 
+  loadedTest1: string;
+
+  private sectionCollection:AngularFirestoreCollection<Section>; //Will contain the reference to our Sections Collection
+  private sections: Observable<Section[]>;  //Will contain all the data of our Section documents within our Sections Collection
+
+  constructor(private afs: AngularFirestore, private instructionsService: InstructionsService, private testsService: TestsService, private testPackageService: TestPackageService) { }
+
+  //Set the values of the section collection and the sections within it based on what test the user clicked in the previous page
+  setSections() {
+    // this.sectionCollection = this.afs.collection<TestPackage>('Demo Exam').doc(si8cxqaQagFFDURRkmY2).collection<Test>('Tests').doc(U09nqinI4BuddNxoSvbW).collection{'Sections'};
+    // this.sectionCollection = this.afs.collection<TestPackage>(this.testsService.loadedExam1.id).doc(this.instructionsService.loadedPackageId1).collection<Test>('Tests').doc(this.loadedTest1).collection<Section>('Sections');
+
+    this.sectionCollection = this.afs.collection<TestPackage>(this.testsService.loadedExam1.id).doc(this.instructionsService.loadedPackageId1).collection<Test>('Tests').doc(this.loadedTest1).collection<Section>('Sections');
+    this.sections = this.sectionCollection.snapshotChanges().pipe(
+      map(sections => {
+        return sections.map(section => {
+          const data = section.payload.doc.data();  // Getting the data of each section
+          const id = section.payload.doc.id;  // Getting the id of each section
+          return { id, ...data };
+        })
+      })
+    )
+   }
+
+   //To View the array of Section documents in the collection
+   getSections(): Observable<Section[]> {
+    return this.sections;
+  }
+
+ //To View the data in a particular Section document
+ getSection(id: string): Observable<Section> {
+   return this.sectionCollection.doc<Section>(id).valueChanges().pipe(
+     take(1),
+     map(tst => {
+       tst.id = id;
+       return tst;
+     })
+   );
+ }
+
+}
